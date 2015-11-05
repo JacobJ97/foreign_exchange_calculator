ae73372d0f560dcc8d1c4fc5bbb92f08da80b2c1
JacobJ97 <jacobjackson977@hotmail.com>
2015-11-05 21:36:37 +1000

    added variable rounding added text input boxes disabling added erasing of other inputbox if one inputbox is being written into

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 __author__ = 'Jake'
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import ListProperty
 from kivy.core.window import Window
-from kivy.uix.textinput import TextInput
 from trip import Details
 import currency
 import time
@@ -11,7 +9,7 @@ import os.path
 
 
 class ForeignExchangeCalculator(App):
-    country_name_codes = ([])
+    country_name_codes = ""
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -24,8 +22,8 @@ class ForeignExchangeCalculator(App):
         Window.size = (500, 700)
 
         # text input disable
-        # self.root.ids.input_country_amount.disabled = True
-        # self.root.ids.input_home_country_amount.disabled = True
+        self.root.ids.input_country_amount.disabled = True
+        self.root.ids.input_home_country_amount.disabled = True
 
         # status label - config file
         if not os.path.isfile('config.txt'):
@@ -72,19 +70,21 @@ class ForeignExchangeCalculator(App):
             country_details_separated = country_details[b]
             country_details_split = country_details_separated.rstrip("\n").split(",")
             self.trip_details.add(country_details_split[0], country_details_split[1], country_details_split[2])
-        current_location = self.trip_details.current_country(current_time)
-        self.root.ids.current_destination_label.text += current_location
+        self.current_location = self.trip_details.current_country(current_time)
+        self.root.ids.current_destination_label.text += self.current_location
         return self.root
-
-    # def change_state(self):
-    #     """ handle change of spinner selection, output result to label widget """
-    #     self.root.ids.country_selection.text = country_list_dictionary[self.country_name_code]
-    #     print("changed to", self.country_list_dictionary[self.country_name_code])
 
     def update_currency(self):
         # text input enable
-        # self.root.ids.input_country_amount.disabled = False
-        # self.root.ids.input_home_country_amount.disabled = False
+        if self.root.ids.input_country_amount.disabled is True and self.root.ids.input_home_country_amount.disabled is True:
+            self.root.ids.input_country_amount.disabled = False
+            self.root.ids.input_home_country_amount.disabled = False
+            self.root.ids.input_country_amount.text = ''
+            self.root.ids.input_home_country_amount.text = ''
+            return
+
+        self.root.ids.input_country_amount.focus = False
+        self.root.ids.input_home_country_amount.focus = False
 
         # currency exchange
         self.currency1 = self.root.ids.input_home_country_amount.text
@@ -93,19 +93,31 @@ class ForeignExchangeCalculator(App):
         # spinner / home information grabbing
         self.country1 = self.root.ids.home_country_label.text
         self.country2 = self.root.ids.country_selection.text
+
+        if self.country2 is "":
+            self.country2 = self.root.ids.country_selection.text = self.current_location
+
         self.country_details1 = currency.get_details(self.country1)
         self.country_details2 = currency.get_details(self.country2)
 
         if self.currency1 is "":
             self.amount2 = currency.convert(self.currency2, self.country_details2[1], self.country_details1[1])
             print(self.amount2)
+            self.amount2 = round(self.amount2, 3)
+            if self.amount2 is -1:
+                self.root.ids.input_country_amount.disabled = True
+                self.root.ids.input_home_country_amount.disabled = True
             self.root.ids.input_home_country_amount.text = str(self.amount2)
             current_time = time.strftime("%H:%M:%S")
             self.root.ids.status.text = "updated at {}".format(current_time)
 
         elif self.currency2 is "":
             self.amount1 = currency.convert(self.currency1, self.country_details1[1], self.country_details2[1])
+            self.amount1 = round(self.amount1, 3)
             print(self.amount1)
+            if self.amount1 is -1:
+                self.root.ids.input_country_amount.disabled = True
+                self.root.ids.input_home_country_amount.disabled = True
             self.root.ids.input_country_amount.text = str(self.amount1)
             current_time = time.strftime("%H:%M:%S")
             self.root.ids.status.text = "updated at {}".format(current_time)
@@ -114,17 +126,34 @@ class ForeignExchangeCalculator(App):
         self.currency1 = self.root.ids.input_home_country_amount.text
         self.currency2 = self.root.ids.input_country_amount.text
 
+        self.root.ids.input_country_amount.focus = False
+        self.root.ids.input_home_country_amount.focus = False
+
         if self.currency1 is "":
             self.amount2 = currency.convert(self.currency2, self.country_details2[1], self.country_details1[1])
+            self.amount2 = round(self.amount2, 3)
             print(self.amount2)
+            if self.amount2 is -1:
+                self.root.ids.input_country_amount.disabled = True
+                self.root.ids.input_home_country_amount.disabled = True
             self.root.ids.input_home_country_amount.text = str(self.amount2)
             self.root.ids.status.text = "{}({}) -> {}({})".format(self.country_details2[1],  self.country_details2[2], self.country_details1[1], self.country_details1[2])
 
         elif self.currency2 is "":
             self.amount1 = currency.convert(self.currency1, self.country_details1[1], self.country_details2[1])
+            self.amount1 = round(self.amount1, 3)
             print(self.amount1)
+            if self.amount1 is -1:
+                self.root.ids.input_country_amount.disabled = True
+                self.root.ids.input_home_country_amount.disabled = True
             self.root.ids.input_country_amount.text = str(self.amount1)
             self.root.ids.status.text = "{}({}) -> {}({})".format(self.country_details1[1],  self.country_details1[2],  self.country_details2[1], self.country_details2[2])
+
+    def clear_textinput(self):
+        if self.root.ids.input_country_amount.focus is True:
+            self.root.ids.input_home_country_amount.text = ''
+        elif self.root.ids.input_home_country_amount.focus is True:
+            self.root.ids.input_country_amount.text = ''
 
 
 
